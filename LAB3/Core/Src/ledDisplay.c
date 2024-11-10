5c4f2f22fb3e6ae5a312f59e6d4e332bbec17b87
unknown <nguyen.hadtscvd554@hcmut.edu.vn>
2024-11-10 21:12:25 +0700

    final

--- LAB3/Core/Src/ledDisplay.c
+++ LAB3/Core/Src/ledDisplay.c
@@ -108,10 +108,15 @@ void display7SEG(int counter){
 enum LedState{RED, YELLOW, GREEN};
 
 enum LedState state = RED;
+enum LedState state2 = GREEN;
+
 int counter = RED_VALUE;
+int count2 = GREEN_VALUE;
 
 void resetCountValue(){
 	counter = red_value;
+	count2 = green_value;
+	state2 = GREEN;
 	state = RED;
 }
 
@@ -146,8 +151,27 @@ int getLed1Value(){
 	return 0;
 }
 
+int getLed2Value(){
+	switch(mode){
+		case 1:
+			return count2;
+			break;
+		case 2:
+			return red_buffer;
+			break;
+		case 3:
+			return yellow_buffer;
+		case 4:
+			return green_buffer;
+		default:
+			break;
+	}
+	return 0;
+}
+
 void normalMode(){
 	counter--;
+	count2--;
 	switch(state){
 		case RED:
 			HAL_GPIO_WritePin(R1_GPIO_Port, R1_Pin, RESET);
@@ -178,11 +202,52 @@ void normalMode(){
 			HAL_GPIO_WritePin(Y1_GPIO_Port, Y1_Pin, SET);
 			HAL_GPIO_WritePin(G1_GPIO_Port, G1_Pin, RESET);
 			if(counter < 0){
-				counter = green_value;
+				counter = red_value;
 				state = RED;
 				HAL_GPIO_WritePin(R1_GPIO_Port, R1_Pin, RESET);
 				HAL_GPIO_WritePin(Y1_GPIO_Port, Y1_Pin, SET);
 				HAL_GPIO_WritePin(G1_GPIO_Port, G1_Pin, SET);
+			}
+			break;
+		default:
+			break;
+	}
+
+	switch(state2){
+		case RED:
+			HAL_GPIO_WritePin(R2_GPIO_Port, R2_Pin, RESET);
+			HAL_GPIO_WritePin(Y2_GPIO_Port, Y2_Pin, SET);
+			HAL_GPIO_WritePin(G2_GPIO_Port, G2_Pin, SET);
+			if(count2 < 0){
+				count2 = yellow_value;
+				state2 = YELLOW;
+				HAL_GPIO_WritePin(R2_GPIO_Port, R2_Pin, SET);
+				HAL_GPIO_WritePin(Y2_GPIO_Port, Y2_Pin, RESET);
+				HAL_GPIO_WritePin(G2_GPIO_Port, G2_Pin, SET);
+			}
+			break;
+		case YELLOW:
+			HAL_GPIO_WritePin(R2_GPIO_Port, R2_Pin, SET);
+			HAL_GPIO_WritePin(Y2_GPIO_Port, Y2_Pin, RESET);
+			HAL_GPIO_WritePin(G2_GPIO_Port, G2_Pin, SET);
+			if(count2 < 0){
+				count2 = green_value;
+				state2 = GREEN;
+				HAL_GPIO_WritePin(R2_GPIO_Port, R2_Pin, SET);
+				HAL_GPIO_WritePin(Y2_GPIO_Port, Y2_Pin, SET);
+				HAL_GPIO_WritePin(G2_GPIO_Port, G2_Pin, RESET);
+			}
+			break;
+		case GREEN:
+			HAL_GPIO_WritePin(R2_GPIO_Port, R2_Pin, SET);
+			HAL_GPIO_WritePin(Y2_GPIO_Port, Y2_Pin, SET);
+			HAL_GPIO_WritePin(G2_GPIO_Port, G2_Pin, RESET);
+			if(count2 < 0){
+				count2 = red_value;
+				state2 = RED;
+				HAL_GPIO_WritePin(R2_GPIO_Port, R2_Pin, RESET);
+				HAL_GPIO_WritePin(Y2_GPIO_Port, Y2_Pin, SET);
+				HAL_GPIO_WritePin(G2_GPIO_Port, G2_Pin, SET);
 			}
 			break;
 		default:
@@ -204,6 +269,9 @@ void ledDisplay(){
 				HAL_GPIO_TogglePin(R1_GPIO_Port, R1_Pin);
 				HAL_GPIO_WritePin(Y1_GPIO_Port, Y1_Pin, SET);
 				HAL_GPIO_WritePin(G1_GPIO_Port, G1_Pin, SET);
+				HAL_GPIO_TogglePin(R2_GPIO_Port, R2_Pin);
+				HAL_GPIO_WritePin(Y2_GPIO_Port, Y2_Pin, SET);
+				HAL_GPIO_WritePin(G2_GPIO_Port, G2_Pin, SET);
 				setTimer0(200);
 			}
 			break;
@@ -212,6 +280,9 @@ void ledDisplay(){
 				HAL_GPIO_TogglePin(Y1_GPIO_Port, Y1_Pin);
 				HAL_GPIO_WritePin(R1_GPIO_Port, R1_Pin, SET);
 				HAL_GPIO_WritePin(G1_GPIO_Port, G1_Pin, SET);
+				HAL_GPIO_TogglePin(Y2_GPIO_Port, Y2_Pin);
+				HAL_GPIO_WritePin(R2_GPIO_Port, R2_Pin, SET);
+				HAL_GPIO_WritePin(G2_GPIO_Port, G2_Pin, SET);
 				setTimer0(200);
 			}
 			break;
@@ -220,6 +291,9 @@ void ledDisplay(){
 				HAL_GPIO_TogglePin(G1_GPIO_Port, G1_Pin);
 				HAL_GPIO_WritePin(Y1_GPIO_Port, Y1_Pin, SET);
 				HAL_GPIO_WritePin(R1_GPIO_Port, R1_Pin, SET);
+				HAL_GPIO_TogglePin(G2_GPIO_Port, G2_Pin);
+				HAL_GPIO_WritePin(Y2_GPIO_Port, Y2_Pin, SET);
+				HAL_GPIO_WritePin(R2_GPIO_Port, R2_Pin, SET);
 				setTimer0(200);
 			}
 			break;
@@ -235,6 +309,8 @@ void update7SEG(){
 			HAL_GPIO_WritePin(MODE_GPIO_Port, MODE_Pin, RESET);
 			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
 			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN2_Pin, RESET);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN3_Pin, RESET);
 			display7SEG(getLed1Value() / 10);
 			scan = 1;
 			break;
@@ -242,13 +318,36 @@ void update7SEG(){
 			HAL_GPIO_WritePin(MODE_GPIO_Port, MODE_Pin, RESET);
 			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
 			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN2_Pin, RESET);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN3_Pin, RESET);
 			display7SEG(getLed1Value() % 10);
 			scan = 2;
 			break;
 		case 2:
+			HAL_GPIO_WritePin(MODE_GPIO_Port, MODE_Pin, RESET);
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN2_Pin, SET);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN3_Pin, RESET);
+			display7SEG(getLed2Value() / 10);
+			scan = 3;
+			break;
+		case 3:
+			HAL_GPIO_WritePin(MODE_GPIO_Port, MODE_Pin, RESET);
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN2_Pin, RESET);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN3_Pin, SET);
+			display7SEG(getLed2Value() % 10);
+			scan = 4;
+			break;
+
+		case 4:
 			HAL_GPIO_WritePin(MODE_GPIO_Port, MODE_Pin, SET);
 			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
 			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN2_Pin, RESET);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN3_Pin, RESET);
 			display7SEG(mode);
 			scan = 0;
 			break;
